28b409bfb5992d5b5547bbf465d768cd1bcc2f60
Pawel Tymoczko <pawelt140@gmail.com>
2022-08-18 20:06:01 +0200

    Add round key & Sub Bytes functions, including Microchip test project

--- AES_Project/AES_Project/aes_encryption.c
+++ AES_Project/AES_Project/aes_encryption.c
@@ -1,13 +1,14 @@
 #pragma once
 #include <avr/pgmspace.h>
 #include <stdint.h>
+#include <string.h>
 #include "aes_encryption.h"
+
 
 void expand_key(uint8_t* key, uint8_t* expanded_key)
 {
 
     uint8_t temp[4];
-    uint8_t sub;
 
     /* Initial copy of original 16 bytes of key */
     memcpy(expanded_key, key, ROUND_KEY_WORDS_NUMBER*4);
@@ -21,7 +22,6 @@ void expand_key(uint8_t* key, uint8_t* expanded_key)
         temp[2] = expanded_key[4*i - 2];
         temp[3] = expanded_key[4*i - 1];
 
-        sub = temp[3];
 
         if (i % ROUND_KEY_WORDS_NUMBER == 0)
         {
@@ -45,12 +45,12 @@ void expand_key(uint8_t* key, uint8_t* expanded_key)
 
 }
 
-void aes_encrypt(uint8_t* expand_key, uint8_t* plain_text, uint8_t encrypted_text)
+void aes_encrypt(uint8_t* expand_key, uint8_t* plain_text, uint8_t* encrypted_text)
 {
 
     /* Initiating */
-    uint8_t round_number = 0;
-    add_round_key(&expand_key, &round_number, &state_matrix);
+    //uint8_t round_number = 0;
+    //add_round_key(&expand_key, &round_number, &state_matrix);
 
     for(uint8_t round_ctr = 0; round_ctr< NUM_ROUNDS; round_ctr++)
     {
@@ -67,20 +67,53 @@ static void add_round_key(uint8_t* expand_key, uint8_t* round_number, uint8_t* s
         return;
     }
 
+    uint8_t round_key[16];
+    /* In case memcpy doesn't work - there is still a loop */
+    memcpy(round_key, expand_key+ROUND_KEY_WORDS_NUMBER* *round_number,ROUND_KEY_WORDS_NUMBER*4);
 
+    for(uint8_t word = 0; word< 16; word++) 
+    {
+        state_matrix[word] ^= round_key[word];
+    }
 
-    /* ADD ROUND KEY NEDD TO BE IMPLEMENTED - keep in mind 4x4 matrix */
 
     *round_number++;
 }
 
-static void substitute_bytes()
+static void substitute_bytes(uint8_t* state_matrix)
 {
-    /* To też nie takie proste */
-    // for(uint8_t i = 0; i<ROUND_KEY_WORDS_NUMBER; i++) 
-    // {
-    //     state_matrix[i] = pgm_read_byte(&sbox[state_matrix[i]]);
-    // }
+    for(uint8_t word = 0; word < 16; word++)
+    {
+        state_matrix[word] = pgm_read_byte(&sbox[state_matrix[word]]);
+    }
 }
-static void shift_raws();
+static void shift_rows(uint8_t* state_matrix)
+{
+    uint8_t temp[4][4];
+
+    /* Trzeba przekminić mocniej */
+    for(uint8_t i =0; i< 4; i++)
+    {
+        for(uint8_t j = 0; j<4; j++)
+        {
+            uint8_t row_number = i/4;
+            if(i % 4 == 1) //row 2
+            {
+
+            }
+
+            else if(i % 4 == 2) //row 3
+            {
+
+            }
+
+            else if(i % 4 == 3) //row 4
+            {
+
+            }
+        }
+        
+    }
+
+}
 static void mix_columns();
---
+++ AES_Project/AES_Project/main.c
@@ -0,0 +1,18 @@
+/*
+ * AES_Project.c
+ *
+ * Created: 18.08.2022 18:41:32
+ * Author : 101
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
